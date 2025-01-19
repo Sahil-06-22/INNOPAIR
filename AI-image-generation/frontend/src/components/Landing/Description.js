38f2551e06b0644f6c863fcf1cb94be903aadb47
Aastha Ahuja <aasthaahuja1200@gmail.com>
2025-01-19 13:48:29 +0800

    Update Description.js

--- AI-image-generation/frontend/src/components/Landing/Description.js
+++ AI-image-generation/frontend/src/components/Landing/Description.js
@@ -7,11 +7,11 @@ export default function Description() {
     justify-center my-24 p-6 md:px-28'>
         <h1 className='text-3xl sm:text-4xl font-semibold
         mb-2'>Create AI Images</h1>
-        <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+        <p className='text-gray-500 mb-8'></p>
     <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
         <img className='w-80 xl:w-96 rounded-lg' src={assets.sample_img_1}/>
         <div>
-            <h2 className='text-3xl font-medium max-w-lg mb-4'>Introducing the AI-Powered Text to Image Generator</h2>
+            <h2 className='text-3xl font-medium max-w-lg mb-4'>YOU IMAGINE. WE CREATE.</h2>
             <p className='text-gray-600 mb-4'>Easily bring your ideas to life with our free
                 AI image generator. Whether you need stunnning
                 visuals or unique imagery, our tool transforms your
