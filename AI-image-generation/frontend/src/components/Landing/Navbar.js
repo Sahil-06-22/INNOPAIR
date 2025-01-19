73cd2ae94eacf123527c3480c74084e2f802c313
Aastha Ahuja <aasthaahuja1200@gmail.com>
2025-01-19 15:08:51 +0800

    Update Navbar.js

--- AI-image-generation/frontend/src/components/Landing/Navbar.js
+++ AI-image-generation/frontend/src/components/Landing/Navbar.js
@@ -37,9 +37,7 @@ export default function Navbar() {
                     </div>
                 ) : (
                     <div className="flex items-center gap-2 sm:gap-5">
-                        <p onClick={() => navigate('/BuyCredit')} className="cursor-pointer">
-                            Pricing
-                        </p>
+                        
                         <button onClick={()=>setShowLogin(true)} className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">Login</button>
 
                     </div>
